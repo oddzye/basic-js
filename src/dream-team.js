ff5b7599fe1e0adca4304d2b9accc5d202613f9e
oddzye <tzariov.dmitry@yandex.ru>
2020-03-15 23:38:29 +0300

    feat: resolve dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,8 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  
+  const sortingMembers = members.filter(item => typeof item === 'string').map(item => item.trim().toLowerCase()).sort();
+  return sortingMembers.reduce((acc, item) => {
+    return `${acc}${item[0].toUpperCase()}`;
+  }, '')
 };
