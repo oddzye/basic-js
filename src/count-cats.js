0ccafc0c39774c54ec374608fcaa4c233e24ae63
oddzye <tzariov.dmitry@yandex.ru>
2020-03-15 23:37:58 +0300

    feat: resolve count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(array) {
+  return array.reduce((acc, item) => {
+    let counter = 0;
+    item.forEach(element => {
+      if (element === '^^') {
+        counter += 1;
+      }
+    })
+    return acc + counter;
+  }, 0)
 };
